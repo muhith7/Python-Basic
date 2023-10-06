702f30827dcfab9b17aefb6ca8c5d6991cac48b7
muhith7 <abdulmuhithnawawi@gmail.com>
2023-10-07 01:52:31 +0700

    Hello World

---
+++ 00-Hello-World/Main.py
@@ -0,0 +1,12 @@
+print("Hello")
+print("World")
+
+print("Hello World!!!")
+
+print("Hello Arlot!!!")
+#Ini adalah comment
+"""Ini adalah comment panjang
+ada dua tipe comment"""
+
+a = 100
+print(a)
